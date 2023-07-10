4742e4efca5f3b68b7e0e5e69fac0a078ec39bd1
ALIM <96762901+mohammed-uwais-ali@users.noreply.github.com>
2023-07-09 20:02:49 -0500

    done with testAutodoc.py

---
+++ testAutodoc.py
@@ -0,0 +1,13 @@
+import unittest
+from autodoc.py import AutoDoc
+
+class testingAutoDoc(unittest.Testcase):
+
+    def testReadFile(self):
+    
+
+        textExpected = "\ndef square(n):\n  return n * n/n/nnumber = float(input("Enter a number: "))/nresult = square(number)"
+        print(testExpected)
+    #test will ensure that each line of the return value is equal
+    def testwriteFile(self):
+        return a-b
