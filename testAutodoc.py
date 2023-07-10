b62318b9263723127ae300bc40c813fb6b8a3bb6
ALIM <96762901+mohammed-uwais-ali@users.noreply.github.com>
2023-07-09 20:01:21 -0500

    Create testAutodoc.py

---
+++ testAutodoc.py
@@ -0,0 +1,36 @@
+# Function: x
+#
+# Parameters:
+# - a: The input value
+#
+# Returns:
+# - The square of the input value
+def x(a):
+
+    return a * a
+
+
+# Function: y
+#
+# Parameters:
+# - a: The first input value
+# - b: The second input value
+#
+# Returns:
+# - The sum of the input values
+def y(a, b):
+
+    return a + b
+
+
+# Function: z
+#
+# Parameters:
+# - a: The first input value
+# - b: The second input value
+#
+# Returns:
+# - The difference between the first and second input values
+def z(a, b):
+
+    return a - b
