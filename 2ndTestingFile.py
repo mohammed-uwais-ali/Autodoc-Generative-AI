40d43525f39f42fbd276116543744a0ff6816299
ALIM <96762901+mohammed-uwais-ali@users.noreply.github.com>
2023-07-09 19:54:11 -0500

    Create 2ndTestingFile.py

---
+++ 2ndTestingFile.py
@@ -0,0 +1,32 @@
+'''
+
+# Function: add2Number
+
+This function takes in two numbers, a and b, and returns their sum.
+
+
+## Parameters:
+
+- self: This parameter is not used within the function.
+- a: The first number
+- b: The second number
+
+
+## Return:
+
+The sum of a and b
+
+
+# Example usage
+
+```python
+number = add2Number(5, 4) # Calling the function with arguments 5 and 4 and \
+storing the result in the variable "number"
+```
+
+Note: The code provided is missing the implementation of the `add2Number` function. \
+In order to use this function, you need to define it first.
+
+'''
+
+# Please add the implementation of the `add2Number` function here.
